98b3da0d8a7361b93a2f97c3770658df2a37ca21
Ian Martin <idanzigm@gmail.com>
2025-05-14 11:20:41 -0400

    restored ingress.yaml and in process of fixing up the setup.bash file

--- setup.bash
+++ setup.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
-aws update-kubeconfig --region us-east-1 --name doggy-app-eks-cluster-0 --profile eks-cluster-admin
+aws eks update-kubeconfig --region us-east-1 --name doggy-app-eks-cluster-0 --profile eks-cluster-admin
 
-kubectl apply -f pods.yaml 
-kubectl apply -f services.yaml 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/examples/2048/ingress.yaml
+kubectl apply -f /home/ec2-user/kubernetesArchitecture/pods.yaml 
+kubectl apply -f /home/ec2-user/kubernetesArchitecture/services.yaml 
+
+kubectl create namespace ingress-nginx
+kubectl apply -f /home/ec2-user/kubernetesArchitecture/ingress.yaml -n ingress-nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.13.0/docs/examples/2048/2048_full.yaml # check here https://github.com/kubernetes-sigs/aws-load-balancer-controller/blob/main/version-stable.txt for latest ersion 
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
